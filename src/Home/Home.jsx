e90350f06810d1d5fd88be9c179dcb68165133e2
Baptiste Ribot <baptisteribot@outlook.fr>
2023-09-18 10:07:33 +0200

    photo, titres

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,3 +1,25 @@
-function Home() {}
+import Nav from "../Nav/Nav.jsx";
+import "./home.css";
+import Profile from "../../public/profil.avif";
+
+function Home() {
+  return (
+    <div>
+      <Nav />
+      <div className="home">
+        <div className="home-text">
+          <h1 className="white">
+            Alicia Smiti <span className="violet">.</span>
+          </h1>
+          <p className="white secondary-title">
+            étudiante en{" "}
+            <span className="violet secondary-title">communication</span>
+          </p>
+        </div>
+        <img src={Profile} alt="Alicia Smiti" className="profile-picture" />
+      </div>
+    </div>
+  );
+}
 
 export default Home;
